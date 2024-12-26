0186fa755168bd0a705ccc519cf09c2f47d0f6bf
emkaliza <novikovaelizaveta987@gmail.com>
2024-12-26 05:38:28 +0200

    Edit Checkout and add Confirmation of Order

--- obj/Debug/net8.0/Plantshop.AssemblyInfo.cs
+++ obj/Debug/net8.0/Plantshop.AssemblyInfo.cs
@@ -16,7 +16,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Plantshop")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6755b68a8e6efbb8b7167585d1fb6633791e20c7")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+785768363caa559a3547a8c879a91b12c4fd4afd")]
 [assembly: System.Reflection.AssemblyProductAttribute("Plantshop")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Plantshop")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
